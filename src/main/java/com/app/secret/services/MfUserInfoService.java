6e84912a0404f85b780eec1281d6e7a512f5aff3
crsu <289435209@qq.com>
2019-12-21 16:37:23 +0800

    "获取员工个人加班时长(未完成

--- src/main/java/com/app/secret/services/MfUserInfoService.java
+++ src/main/java/com/app/secret/services/MfUserInfoService.java
@@ -1,25 +1,6 @@
 package com.app.secret.services;
 
-import com.app.secret.core.dto.GetPersonalOvertimeDTO;
-import com.app.secret.core.vo.PersonaOvertimeVO;
-
-
 public interface MfUserInfoService {
-
-    /**
-     * 获取个人加班时长
-     *
-     * @param query
-     * @return
-     */
-    PersonaOvertimeVO getPersonalOvertime(GetPersonalOvertimeDTO query);
-
-    /**
-     * 判断员工是否为IT人员
-     *
-     * @param pcode
-     * @return
-     */
-    boolean judgeIT(String pcode);
+	
 }
 
